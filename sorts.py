4e3753dbd40b0d642adfeb66edfada5e98628283
Navaneeth <zorbhathebuddha@gmail.com>
2024-10-31 19:55:21 +0800

    binary search array or list in py

--- sorts.py
+++ sorts.py
@@ -10,5 +10,21 @@ def insertion_sort(nums: List[int]) -> List[int]:
     return nums
 
 
-
 print(insertion_sort([3, 54, 48, 1, 9]))
+
+
+def search_array(nums: List[int], target: int) -> int:
+    left = nums[0]
+    right = len(nums) - 1
+    mid = (left + right) // 2
+    while left <= right:
+        if nums[mid] > target:
+            right = mid - 1
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            return mid
+    return -1
+
+
+print(search_array([4, 5, 6, 77, 188, 200, 455], 200))
