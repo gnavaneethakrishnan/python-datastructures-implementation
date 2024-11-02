ea5b3c8df84bb620722413107ee331f63e050171
Navaneeth <zorbhathebuddha@gmail.com>
2024-11-02 18:15:59 +0800

    added selection and bubble sort

--- sorts.py
+++ sorts.py
@@ -13,6 +13,31 @@ def insertion_sort(nums: List[int]) -> List[int]:
 print(insertion_sort([3, 54, 48, 1, 9]))
 
 
+def bubble_sort(nums: List[int]) -> List[int]:
+    for i in range(len(nums) - 1, 0, -1):
+        for j in range(i):
+            if nums[j] > nums[j + 1]:
+                nums[j + 1], nums[j] = nums[j], nums[j + 1]
+    return nums
+
+
+print(bubble_sort([3, 5, 10, 98, 45, 1, 100]))
+
+
+def selection_sort(nums: List[int]) -> List[int]:
+    for i in range(len(nums)-1):
+        min_index: int = i
+        for j in range(i+1, len(nums)):
+            if nums[j] < nums[min_index]:
+                min_index = j
+        if min_index != i:
+            nums[i], nums[min_index] = nums[min_index], nums[i]
+    return nums
+
+
+print(selection_sort([100, 6, 90, 1, 88]))
+
+
 def search_array(nums: List[int], target: int) -> int:
     left = nums[0]
     right = len(nums) - 1
