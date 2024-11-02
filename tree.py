f6ff3c3092a98d590fa26e4dabefc6c95d1bf459
Navaneeth <zorbhathebuddha@gmail.com>
2024-11-02 12:04:40 +0800

    added idea folder and addeed recurrsive methods

--- tree.py
+++ tree.py
@@ -49,13 +49,74 @@ class BinarySearchTree:
             else:
                 return True
 
+    def __r_contains(self, current_node: Node, value: int) -> bool:
+
+        if current_node is None:
+            return False
+        if current_node.value == value:
+            return True
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        elif value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+
+    def r_contains(self, value: int) -> bool:
+        return self.__r_contains(self.root, value)
+
+    def __r_insert(self, current_node: Node, value: int) -> None | Node:
+
+        if current_node is None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value)
+        return current_node
+
+    def r_insert(self, value: int) -> None:
+        if self.root is None:
+            self.root = Node(value)
+        self.__r_insert(self.root, value)
+
+    def minimum_value(self, current_node: Node) -> int:
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
+    def __delete_(self, current_node: Node, value: int) -> Node | None:
+
+        if current_node is None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_(current_node.right, value)
+        else:
+            if current_node.left is None and current_node.right is None:
+                return None
+            elif current_node.left is None:
+                current_node = current_node.right
+            elif current_node.right is None:
+                current_node = current_node.left
+            else:
+                min_value = self.minimum_value(current_node.right)
+                current_node.value = min_value
+                current_node.right = self.__delete_(current_node.right, min_value)
+        return current_node
+
+    def delete(self, value: int) -> Node | None:
+        if self.root is None:
+            return None
+        return self.__delete_(self.root, value)
+
 
 if __name__ == "__main__":
     my_tree: BinarySearchTree = BinarySearchTree()
     print(my_tree.root)
-    my_tree.insert__(10)
-    my_tree.insert__(11)
-    print(my_tree.root.right.value)  # type: ignore
+    my_tree.r_insert(10)
+    print(my_tree.root.value)
+    my_tree.r_insert(11)
+    print(my_tree.root.right.value)
     my_tree.insert__(9)
     my_tree.insert__(12)
     print(my_tree.root.right.right.value)  # type: ignore
