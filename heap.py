7b2acdacd4c8485954f6594a73d0db8ee041a51c
Navaneeth <zorbhathebuddha@gmail.com>
2024-11-03 11:10:52 +0800

    fixed heap

--- heap.py
+++ heap.py
@@ -6,7 +6,7 @@ class MaxHeap:
         return 2 * index + 1
 
     def _right_child(self, index: int) -> int:
-        return 2 * index + 1
+        return 2 * index + 2
 
     def _parent(self, index: int) -> int:
         return (index - 1) // 2
@@ -18,7 +18,7 @@ class MaxHeap:
         self.heap.append(value)
         current: int = len(self.heap) - 1
 
-        while current < 0 and self.heap[current] > self.heap[self._parent(current)]:
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
@@ -28,9 +28,9 @@ class MaxHeap:
         while True:
             left_index: int = self._left_child(index)
             right_index: int = self._right_child(index)
-            if (self.heap[left_index] < len(self.heap)) and self.heap[left_index] > self.heap[max_index]:
+            if (left_index < len(self.heap)) and self.heap[left_index] > self.heap[max_index]:
                 max_index = left_index
-            if (self.heap[left_index] < len(self.heap)) and self.heap[right_index] > self.heap[max_index]:
+            if (right_index < len(self.heap)) and self.heap[right_index] > self.heap[max_index]:
                 max_index = right_index
             if max_index != index:
                 self._swap(index, max_index)
@@ -39,7 +39,38 @@ class MaxHeap:
                 return
 
     def _remove(self) -> int:
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
         max_value: int = self.heap[0]
         self.heap[0] = self.heap.pop()
-        self._sink_down(max_value, 0)
+        self._sink_down(0)
         return max_value
+
+my_heap = MaxHeap()
+print(my_heap.heap)
+my_heap._insert(99)
+my_heap._insert(72)
+my_heap._insert(61)
+my_heap._insert(58)
+
+print(my_heap.heap)
+my_heap._insert(100)
+print(my_heap.heap)
+my_heap._insert(75)
+print(my_heap.heap)
+
+my_heap1 = MaxHeap()
+my_heap1._insert(95)
+my_heap1._insert(75)
+my_heap1._insert(80)
+my_heap1._insert(55)
+my_heap1._insert(60)
+my_heap1._insert(50)
+my_heap1._insert(65)
+print(my_heap1.heap)
+print(my_heap1._remove())
+print(my_heap1.heap)
+
+
